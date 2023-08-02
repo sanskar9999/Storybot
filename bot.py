b354cbebade3c3b0d40a33d5e211eec25106b6aa
npc <115063350+sanskar9999@users.noreply.github.com>
2023-08-02 14:32:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ story = [
         "images": []
     },
     {
-        "text": "You wait until morning, but, as the rosy wisps of dawn begin to light the eastern sky, a chill and forbidding wind begins to blow.If you seek shelter, turn to page 6.If you brave the freezing wind to see more of the world about you, tum to page 16.",
+        "text": "You wait until morning, but, as the rosy wisps of dawn begin to light the eastern sky, a chill and forbidding wind begins to blow.If you seek shelter, .If you brave the freezing wind to see more of the world about you,",
         "options": {
             "\U0001F1E6": "If you seek shelter",
             "\U0001F1E7": "If you brave the freezing wind to see more of the world about you"
@@ -39,7 +39,7 @@ story = [
         "images": []
     },
     {
-        "text": "You step into a niche in the rocks to escape the merciless blast of wind and lean back against the rock wall that crumbles under your weight, causing you to fall backward down a muddy slope and into a pond. The sun shines brightly down on you as you pick yourself up, dripping wet, and wade to the grassy shore. You look back at the rock, rising out of the pond, but you can’t see where you fell through.While you are collecting your senses, a horse comes prancing up, its rider dressed in tin armor—a knight out of the history books—enough to make you laugh. The horseman lifts off his helmet and laughs himself. “What a place for a bath!” he calls out. “Well, it was worth it—you’re cleaner than a pig!” He al­ most falls off his horse, he is laughing so hard. “But climb on and I’ll take you back to the castle,” he says. “We’ll see if we can’t make a human out of you yet.”If you accept the ride back to the castle, tum to page 22.If you decline the invitation and try to find your way back into the Cave of Time, tum to page 114.",
+        "text": "You step into a niche in the rocks to escape the merciless blast of wind and lean back against the rock wall that crumbles under your weight, causing you to fall backward down a muddy slope and into a pond. The sun shines brightly down on you as you pick yourself up, dripping wet, and wade to the grassy shore. You look back at the rock, rising out of the pond, but you can’t see where you fell through.While you are collecting your senses, a horse comes prancing up, its rider dressed in tin armor—a knight out of the history books—enough to make you laugh. The horseman lifts off his helmet and laughs himself. “What a place for a bath!” he calls out. “Well, it was worth it—you’re cleaner than a pig!” He al­ most falls off his horse, he is laughing so hard. “But climb on and I’ll take you back to the castle,” he says. “We’ll see if we can’t make a human out of you yet.”If you accept the ride back to the castle.If you decline the invitation and try to find your way back into the Cave of Time",
         "options": {
             "\U0001F1E6": "If you accept the ride back to the castle",
             "\U0001F1E7": "If you decline the invitation and try to find your way back into the Cave of Time"  
@@ -47,7 +47,7 @@ story = [
         "images": []
     },
     {
-        "text": "The horseman helps you up on his horse and you sit uncomfortably as it canters over the countryside. After traveling a mile or so, you come to a great, stone castle. The horse trots across the drawbridge and into the stable. “Jump,” the knight calls to you, and you slide off the rear of the horse. The knight escorts you into the grand chamber of the castle. All about you are stewards, attendants, and knights. A few min­ utes later you find yourself bowing before the King himself.After hearing your story, the King looks gravely at his advisors and knights and stewards. “Does anyone believe this tale?” he asks.Everyone cries back, “No, Your Majesty,” or “Certainly not, Your Majesty.”“Then tell us the truth!” the King roars at you.If you insist you are telling the truth, tum to page 36.If you try to make up a plausible story, tum to page 37.",
+        "text": "The horseman helps you up on his horse and you sit uncomfortably as it canters over the countryside. After traveling a mile or so, you come to a great, stone castle. The horse trots across the drawbridge and into the stable. “Jump,” the knight calls to you, and you slide off the rear of the horse. The knight escorts you into the grand chamber of the castle. All about you are stewards, attendants, and knights. A few min­ utes later you find yourself bowing before the King himself.After hearing your story, the King looks gravely at his advisors and knights and stewards. “Does anyone believe this tale?” he asks.Everyone cries back, “No, Your Majesty,” or “Certainly not, Your Majesty.”“Then tell us the truth!” the King roars at you.If you insist you are telling the truth If you try to make up a plausible story   ",
         "options": {
             "\U0001F1E6": "If you insist you are telling the truth",
             "\U0001F1E7": "If you try to make up a plausible story"
@@ -55,21 +55,21 @@ story = [
         "images": []
     },
     {
-        "text": "\"I know it sounds strange, Your Majesty,\" you say, \"but I have no reason to incur your wrath by making up a false story.\" The King looks around at his courtiers. They all have grave expressions on their faces, as if you have committed some unpardonable sin. Turn to page 11.",
+        "text": "\"I know it sounds strange, Your Majesty,\" you say, \"but I have no reason to incur your wrath by making up a false story.\" The King looks around at his courtiers. They all have grave expressions on their faces, as if you have committed some unpardonable sin ",
         "options": {
             "\U0001F1E6": "..."
         },
         "images": []
     },
     {
-        "text": "“I’m sorry to have intruded upon your royal domain, Your Majesty.’’ you say humbly, trying to think up a good story as fast as you can. “It is true, sire, I have been badly mistreated by my wicked stepfather, with whom I live, and I place myself under your wise and just protection.’’“Who is this wicked stepfather and where does he live?” the King asks. “If he is wicked enough, we may want him to be one of our knights,” he adds, laughing, as do all the courtiers.“He lives beyond that hill,” you say, pointing toward a high wooded ridge, “and his name is Smith.”The King laughs once again. “Then your step­ father must be a fish,” he says, “for beyond yon­ der hill is Loch Ness.” Turn to page 11.",
+        "text": "“I’m sorry to have intruded upon your royal domain, Your Majesty.’’ you say humbly, trying to think up a good story as fast as you can. “It is true, sire, I have been badly mistreated by my wicked stepfather, with whom I live, and I place myself under your wise and just protection.’’“Who is this wicked stepfather and where does he live?” the King asks. “If he is wicked enough, we may want him to be one of our knights,” he adds, laughing, as do all the courtiers.“He lives beyond that hill,” you say, pointing toward a high wooded ridge, “and his name is Smith.”The King laughs once again. “Then your step­ father must be a fish,” he says, “for beyond yon­ der hill is Loch Ness.”  ",
         "options": {
             "\U0001F1E6": "...."
         },
         "images": []
     },
     {
-        "text": "'Off to the tower,' the King shouts. Two knights leap forward, drag you out of the chamber, and, with spears at your back, force you to climb forty­ eight stone steps to the tower prison—a tiny cylin­ drical room with one small window looking out over the moat and pasture land beyond. The only furniture is a bed of straw.You realize you are back in the early days of feudal Europe, where the only laws are the King's whims. You have no idea how long he intends to keep you in the tower. There is one possibility of escape. The water in the moat, about twenty-five feet almost directly below your window, is quite deep. If you jump out far enough, you should land in the deep water and not be hurt. If you jump, turn to page 12. If not, turn to page 13.",
+        "text": "'Off to the tower,' the King shouts. Two knights leap forward, drag you out of the chamber, and, with spears at your back, force you to climb forty­ eight stone steps to the tower prison—a tiny cylin­ drical room with one small window looking out over the moat and pasture land beyond. The only furniture is a bed of straw.You realize you are back in the early days of feudal Europe, where the only laws are the King's whims. You have no idea how long he intends to keep you in the tower. There is one possibility of escape. The water in the moat, about twenty-five feet almost directly below your window, is quite deep. If you jump out far enough, you should land in the deep water and not be hurt. If you jump,  If not, ",
         "options": {
             "\U0001F1E6": "If you jump",
             "\U0001F1E7": "If not"   
@@ -77,7 +77,7 @@ story = [
         "images": []
     },
     {
-        "text": "You jump far out and fall faster and faster. You enter the water feet first and hit bottom, but the soft mud receives you gently. In a few seconds you reach the surface. You swim to the outer banks of the moat, shaken but unharmed. You scramble up the bank and run for the cover of the forest.You walk along the edge of the forest until well out of sight of the castle, then head across the open countryside. You stop a peasant to ask him where you might stay for the night. “Walk up that hill and you’ll see before you the waters of Loch Ness,” he says. “You’ll find a place there.”You follow his directions and, seeing some little houses near the lake, proceed toward them. Dark­ ness is setting in, and you are glad when you meet a fisherman who says he will give you shelter for the night. He and his wife are kindly people; they invite you to stay and earn your keep by helping them fish. If you accept, tum to page 66. If you decide to travel on, tum to page 78.",
+        "text": "You jump far out and fall faster and faster. You enter the water feet first and hit bottom, but the soft mud receives you gently. In a few seconds you reach the surface. You swim to the outer banks of the moat, shaken but unharmed. You scramble up the bank and run for the cover of the forest.You walk along the edge of the forest until well out of sight of the castle, then head across the open countryside. You stop a peasant to ask him where you might stay for the night. “Walk up that hill and you’ll see before you the waters of Loch Ness,” he says. “You’ll find a place there.”You follow his directions and, seeing some little houses near the lake, proceed toward them. Dark­ ness is setting in, and you are glad when you meet a fisherman who says he will give you shelter for the night. He and his wife are kindly people; they invite you to stay and earn your keep by helping them fish. If you accept  . If you decide to travel on  ",
         "options": {
             "\U0001F1E6": "If you accept",
             "\U0001F1E7": "If you decide to travel on"
@@ -115,7 +115,7 @@ story = [
         "images": []
     },  
     {
-        "text": "You follow the left passageway. It leads upward to the surface. Before you, a grassy meadow slopes down to a clear, fast-flowing stream; be­ yond it are pine-covered foothills stretching in the distance toward snow-covered peaks. You might be in Wyoming in your own time, but, whatever time it is, the world you see appears to be a hospi­ table one. You notice a herd of buffalo grazing. But nowhere can you see a house, or fence, or road, or any sign of human presence. It is possible you are living hundreds, perhaps thousands, of years ago. You gaze upward. One of the puffy, white cumulus clouds is moving in a strange fashion. It is descending! A spaceship is landing right before your eyes, only a few hundred yards away! If you hide from view, tum to page 38. If S° UP 1° the spaceship, tum to page 40.",
+        "text": "You follow the left passageway. It leads upward to the surface. Before you, a grassy meadow slopes down to a clear, fast-flowing stream; be­ yond it are pine-covered foothills stretching in the distance toward snow-covered peaks. You might be in Wyoming in your own time, but, whatever time it is, the world you see appears to be a hospi­ table one. You notice a herd of buffalo grazing. But nowhere can you see a house, or fence, or road, or any sign of human presence. It is possible you are living hundreds, perhaps thousands, of years ago. You gaze upward. One of the puffy, white cumulus clouds is moving in a strange fashion. It is descending! A spaceship is landing right before your eyes, only a few hundred yards away! If you hide from view . If S° UP 1° the spaceship .",
         "options": {
             "\U0001F1E6": "If you hide from view",
             "\U0001F1E7": "If S° UP 1° the spaceship"
@@ -123,7 +123,7 @@ story = [
         "images": []
     },
     {
-        "text": "You walk along the right-hand passageway for a long distance, suddenly you are sliding. Your head strikes something and you are knocked unconscious. When you wake up, you find yourself by a small lake, bordered by woods. A boy about twelve years old is fishing nearby, but there is no one else in sight. You go up and introduce yourself, hoping you can find out what year it is without sounding crazy. Fortunately, the boy is friendly and good na- tured. He tells you his name is Nick Tyler and that he lives on Birch Street. He works in his father's business making candles and soap—the best in the Colonies, he says. If you try to make up a believable story, turn to page 106. If you tell him you come from the twentieth Century through the Cave of Time, Nick smiles. Then you tell him a little about life in your own time—about cars and planes, telephones and tele­ vision. He listens intently, with a big grin on his face, as if you are telling th funniest story ever told.",
+        "text": "You walk along the right-hand passageway for a long distance, suddenly you are sliding. Your head strikes something and you are knocked unconscious. When you wake up, you find yourself by a small lake, bordered by woods. A boy about twelve years old is fishing nearby, but there is no one else in sight. You go up and introduce yourself, hoping you can find out what year it is without sounding crazy. Fortunately, the boy is friendly and good na- tured. He tells you his name is Nick Tyler and that he lives on Birch Street. He works in his father's business making candles and soap—the best in the Colonies, he says. If you try to make up a believable story . If you tell him you come from the twentieth Century through the Cave of Time, Nick smiles. Then you tell him a little about life in your own time—about cars and planes, telephones and tele­ vision. He listens intently, with a big grin on his face, as if you are telling th funniest story ever told.",
         "options": {
             "\U0001F1E6": "If you try to make up a believable story",
             "\U0001F1E7": "If you tell him you come from the Twentieth Century through the Cave of Time, Nick smiles. Then you tell him a little about life in your own time—about cars and planes, telephones and tele­vision. He listens intently, with a big grin on his face, as if you are telling th funniest story ever told."
@@ -131,7 +131,7 @@ story = [
         "images": []
     },
     {
-        "text": "You cautiously approach the spaceship and, to your amazement, see that it is resting a foot or so above the ground, without any visible mechanism keeping it aloft. There are no engines, rocket exhausts, port holes, landing gear, antennae, or any equipment you might imagine a spaceship would need. You realize it must be the product of a supremely advanced civilization. Trusting that such people have learned to be loving toward others, you approach the ship. A portal slides open, but all you can see within is shimmering blue light. A large cube is thrust out through the portal and lowered to the ground by mechanical arms. The top of the cube is with­drawn, leaving a pallet on which lie the sleeping forms of three men and three women, dressed in shrouds of animal skin. Their bodies and features remind you of pictures you have seen depicting the earliest men on earth. You have an impulse to jump aboard the spaceship before the portal closes. If you do, tum to page 115. If not, tum to page 83.",
+        "text": "You cautiously approach the spaceship and, to your amazement, see that it is resting a foot or so above the ground, without any visible mechanism keeping it aloft. There are no engines, rocket exhausts, port holes, landing gear, antennae, or any equipment you might imagine a spaceship would need. You realize it must be the product of a supremely advanced civilization. Trusting that such people have learned to be loving toward others, you approach the ship. A portal slides open, but all you can see within is shimmering blue light. A large cube is thrust out through the portal and lowered to the ground by mechanical arms. The top of the cube is with­drawn, leaving a pallet on which lie the sleeping forms of three men and three women, dressed in shrouds of animal skin. Their bodies and features remind you of pictures you have seen depicting the earliest men on earth. You have an impulse to jump aboard the spaceship before the portal closes. If you do . If not ",
         "options": {
             "\U0001F1E6": "If you do",
             "\U0001F1E7": "If not"
@@ -168,7 +168,7 @@ async def story_help(ctx):
 async def begin_story(ctx):  # Renamed the story command to begin_story
     global is_story_running, story_chapter, story_options
     if is_story_running:
-        await ctx.send("The story is already running. Type !continue to proceed.")
+        await ctx.send("The story is already running.")
     else:
         is_story_running = True
         story_chapter = 0
